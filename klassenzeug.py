36469512a577dfe2c6ec46598406fe98fd2fd3e2
Nicco Kunzmann <niccokunzmann@googlemail.com>
2012-03-19 23:14:06 +0100

    habe tests fuer studentenklasse geschrieben

--- klassenzeug.py
+++ klassenzeug.py
@@ -15,3 +15,12 @@
 
 
 
+hans  = Student('Hans', 'Mayer')
+assert hans.name == 'Hans'
+assert hans.nachname == 'Mayer'
+
+hans.set_alter(42)
+assert hans.get_alter() == 42
+
+hans.altern()
+assert hans.get_alter() == 43
