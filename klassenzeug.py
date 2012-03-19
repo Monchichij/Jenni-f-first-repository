dd9edb7178b7961db3b4bf7a72cb249026a1ddfb
Nicco Kunzmann <niccokunzmann@googlemail.com>
2012-03-19 23:19:17 +0100

    schule hinzugefuegt

--- klassenzeug.py
+++ klassenzeug.py
@@ -24,3 +24,11 @@ assert hans.get_alter() == 42
 
 hans.altern()
 assert hans.get_alter() == 43
+
+schule = Schule('Schule')
+
+assert hans not in schule.schueler
+
+schule.nehme_auf(hans)
+
+assert hans in schule.schueler
